a868d186971ff89348f6f35b8f7d71476cfd07ca
Aviv Cohen <avivcohen22@icloud.com>
2025-11-20 22:44:44 +0200

    refactor
    refactor

--- XO/XO/XOApp.swift
+++ XO/XO/XOApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct XOApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+   var body: some Scene {
+      WindowGroup {
+         ContentView()
+      }
+   }
 }
