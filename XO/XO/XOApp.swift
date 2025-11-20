bcf9ac968e9421e1c5147648268587dc3b4d0276
Aviv Cohen <avivcohen22@icloud.com>
2025-11-20 22:42:07 +0200

    project init
    created xcode project

---
+++ XO/XO/XOApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOApp.swift
+//  XO
+//
+//  Created by Aviv Cohen on 20/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct XOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
