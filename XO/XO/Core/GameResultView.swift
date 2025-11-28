42920cf5933312ff65cf074d77f0eddbc3c37684
Aviv Cohen <avivcohen22@icloud.com>
2025-11-28 21:57:18 +0200

    .
    .

--- XO/XO/Core/GameResultView.swift
+++ XO/XO/Core/GameResultView.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct GameResultView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+   let gameState: GameState
+   let resetGame: () -> ()
+   
+   var body: some View {
+      VStack(spacing: 24) {
+         titleView
+         if let winner = gameState.winnerPlayer {
+            winnerPlayerView(winner)
+         }
+         playAgainButton
+      }
+      .padding()
+      .infinityFrame()
+      .background(Color.appTheme.viewBackground)
+      .presentationDetents([.height(gameConstants.winnerSheetHeight)])
+      .presentationCornerRadius(AppCornerRadius.overall.value)
+      .presentationDragIndicator(.visible)
+   }
 }
-
+   
+private extension GameResultView {
+      var titleView: some View {
+         Text(gameState.isTied ? "🤝 It's a Tie!" : "🎉 Game Over!")
+            .font(.title)
+            .fontWeight(.bold)
+            .foregroundStyle(gameState.isTied ? Color.appTheme.alternateAccent : Color.appTheme.success)
+   }
+   
+   func winnerPlayerView(_ winner: Player) -> some View {
+      HStack(spacing: 8) {
+         Text("Winner:")
+            .font(.title3)
+            .fontWeight(.medium)
+            .foregroundStyle(Color.appTheme.secondaryText)
+         Image(winner.image)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 48, height: 48)
+         Text(winner.cellSymbol.symbol)
+            .font(.title)
+            .fontWeight(.heavy)
+            .fontDesign(.rounded)
+            .foregroundStyle(winner.cellSymbol.color)
+            .padding(.horizontal,12)
+            .padding(.vertical, 4)
+            .background(RoundedRectangle(cornerRadius: AppCornerRadius.overall.value).fill(winner.cellSymbol.color.opacity(0.2)))
+         
+      }
+   }
+   
+   var playAgainButton: some View {
+      Label("Play again", systemImage: "arrow.counterclockwise.circle.fill")
+         .primaryButton()
+         .button(.press) {
+          resetGame()
+         }
+   }
+}
+   
 #Preview {
-    GameResultView()
+   GameResultView(gameState: .won(.defaultPlayer)) {}
+   GameResultView(gameState: .tied) {}
 }
--- XO/XO/Domain/GameState.swift
+++ XO/XO/Domain/GameState.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+enum GameState: Equatable {
+   case playing
+   case won(Player)
+   case tied
+   
+   var isGameOver: Bool {
+      switch self {
+      case .playing: false
+      case .won, .tied: true
+      
+      }
+   }
+   
+   var isTied: Bool {
+      self == .tied
+   }
+   
+   var winnerPlayer: Player? {
+      switch self {
+         case .won(let player): player
+      case .playing, .tied: nil
+      }
+   }
+}
